fbc3e605be5d263406990e2dc1db0275943f7637
达观 <lu873737753@163.COM>
2022-07-17 19:20:19 +0800

    提交测试

---
+++ src/main/java/org/shuijing/shuijingwaimai/ShuijingwaimaiApplication.java
@@ -0,0 +1,13 @@
+package org.shuijing.shuijingwaimai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuijingwaimaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuijingwaimaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/shuijing/shuijingwaimai/ShuijingwaimaiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shuijing.shuijingwaimai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuijingwaimaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
