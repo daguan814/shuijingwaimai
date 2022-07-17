30cbb29266f22e85111b556ffd7101d953bed4a7
达观 <lu873737753@163.COM>
2022-07-18 00:49:10 +0800

    代码生成器

---
+++ src/main/java/org/shuijing/shuijingwaimai/Generator.java
@@ -0,0 +1,67 @@
+package org.shuijing.shuijingwaimai;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.type.filter.AbstractClassTestingTypeFilter;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+/**
+ * @ClassName: Generator
+ * @Description: TODO
+ * @author: 达观
+ * @date: 2022/7/18  0:00
+ */
+public class Generator {
+
+
+    public static void main(String[] args) {
+
+        AutoGenerator autoGenerator = new AutoGenerator();
+        DataSourceConfig dataSource = new DataSourceConfig();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/shuijing");
+        dataSource.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Lhf@2001.");
+
+        autoGenerator.setDataSource(dataSource);
+
+        //设置全局配置
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(System.getProperty("user.dir")+"/shuijingwaimai/src/main/java");    //设置代码生成位置
+        globalConfig.setOpen(false);    //设置生成完毕后是否打开生成代码所在的目录
+        globalConfig.setMapperName("%sDao");    //设置数据层接口名，%s为占位符，指代模块名称
+        globalConfig.setIdType(IdType.ASSIGN_ID);   //设置Id生成策略
+        autoGenerator.setGlobalConfig(globalConfig);
+
+        //设置包名相关配置
+        PackageConfig packageInfo = new PackageConfig();
+        packageInfo.setParent("com.aaa");   //设置生成的包名，与代码所在位置不冲突，二者叠加组成完整路径
+        packageInfo.setEntity("domain");    //设置实体类包名
+        packageInfo.setMapper("dao");   //设置数据层包名
+        autoGenerator.setPackageInfo(packageInfo);
+
+        //策略设置
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setInclude("tbl_user");  //设置当前参与生成的表名，参数为可变参数
+        strategyConfig.setTablePrefix("tbl_");  //设置数据库表的前缀名称，模块名 = 数据库表名 - 前缀名  例如： User = tbl_user - tbl_
+        strategyConfig.setRestControllerStyle(true);    //设置是否启用Rest风格
+        strategyConfig.setVersionFieldName("version");  //设置乐观锁字段名
+        strategyConfig.setLogicDeleteFieldName("deleted");  //设置逻辑删除字段名
+        strategyConfig.setEntityLombokModel(true);  //设置是否启用lombok
+        autoGenerator.setStrategy(strategyConfig);
+        //2.执行生成操作
+
+
+        autoGenerator.execute();
+
+    }
+}
