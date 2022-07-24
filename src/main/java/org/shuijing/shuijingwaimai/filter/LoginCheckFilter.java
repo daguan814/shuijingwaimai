bc3de5d304ef39fe99d254b803cc45485b0c75ff
达观 <lu873737753@163.COM>
2022-07-24 14:14:10 +0800

    添加登录过滤器

---
+++ src/main/java/org/shuijing/shuijingwaimai/filter/LoginCheckFilter.java
@@ -0,0 +1,37 @@
+package org.shuijing.shuijingwaimai.filter;
+
+/**
+ * @ClassName: LoginCheckFilter
+ * @Description: TODO
+ * @author: 达观
+ * @date: 2022/7/24  14:05
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpResponse;
+
+/**
+ * 检查用户是否登录
+ */
+
+
+
+@WebFilter(filterName = "LoginCheckFilter" ,urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        log.info("拦截到请求：{}",request.getRequestURI());
+    }
+}
